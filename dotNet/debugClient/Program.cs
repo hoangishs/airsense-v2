9d0b1c09430ac32afecd3300348e4842725fa6e2
hoangishs <38305536+hoangishs@users.noreply.github.com>
2019-04-01 11:35:34 +0700

    add debugClient

---
+++ dotNet/debugClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             ASCIIEncoding encoding= new ASCIIEncoding();
+            try
+            {
+                TcpClient client = new TcpClient();
+
+                string ipAddress = Console.ReadLine();
+                // 1. connect
+                client.Connect(ipAddress, 8888);
+                Stream stream = client.GetStream();
+
+                Console.WriteLine("Connected to ESP.");
+
+                while (true)
+                {
+                    int data = stream.ReadByte();
+                    if (data>0)
+                    {
+                        Console.Write((char)data);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+
+            Console.Read();
+        }
+    }
+}
